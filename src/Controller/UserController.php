141a4a8c799ca9a87e0b77e0d6413b262fc1da07
Marco Ferraz <marco.ferraz98@outlook.com>
2023-07-25 16:49:07 -0300

    feat: simple crud user and depency inject of interface

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +12,40 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route("/api", name: "api_")]
 class UserController extends AbstractController
 {
-    #[Route('/users', name: 'app_user', methods:['GET'])]
+    #[Route('/users', name: 'app_user', methods: ['GET'])]
     public function index(UserRepository $userRepository): JsonResponse
     {
         return $this->json([
             'data' => $userRepository->findAll()
+        ]);
+    }
+
+    #[Route('/users/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(int $id, UserRepository $userRepository): JsonResponse
+    {
+        $user = $userRepository->find($id);
+        if (!$user) throw $this->createNotFoundException();
+
+        return $this->json([
+            'data' => $user
+        ]);
+    }
+
+    #[Route('/users/{id}', name: 'app_user_update', methods: ['PATCH', 'PUT'])]
+    public function update(int $id, UserRepository $userRepository, Request $request): JsonResponse
+    {
+        $user = $userRepository->find($id);
+        if (!$user) throw $this->createNotFoundException();
+
+        $decoded = json_decode($request->getContent());
+        $user->setRoles($decoded->roles);
+        $user->setUsername($decoded->username);
+        $user->setPassword($decoded->password);
+        $userRepository->add($user, true);
+
+        return $this->json([
+            'message' => 'User updated successfully',
+            'data' => $user
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,19 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -83,7 +92,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setPassword(string $password): static
     {
-        $this->password = $password;
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $this,
+            $password
+        );
+
+        $this->password = $hashedPassword;
 
         return $this;
     }
